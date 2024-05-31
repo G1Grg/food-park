5165b79baf88d029c7ced2a53cc1a47435dac103
Jeevan <53844739+G1Grg@users.noreply.github.com>
2024-05-31 18:12:52 +1000

    The default laravel login page has been changed to the new one.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,8 +50,8 @@
 
 @section('content')
     <!--=============================
-                                                    BREADCRUMB START
-                                                ==============================-->
+                                                                BREADCRUMB START
+                                                            ==============================-->
     <section class="fp__breadcrumb" style="background: url(images/counter_bg.jpg);">
         <div class="fp__breadcrumb_overlay">
             <div class="container">
@@ -66,13 +66,13 @@
         </div>
     </section>
     <!--=============================
-                                                    BREADCRUMB END
-                                                ==============================-->
+                                                                BREADCRUMB END
+                                                            ==============================-->
 
 
     <!--=========================
-                                                    SIGNIN START
-                                                ==========================-->
+                                                                SIGNIN START
+                                                            ==========================-->
     <section class="fp__signin" style="background: url(images/login_bg.jpg);">
         <div class="fp__signin_overlay pt_125 xs_pt_95 pb_100 xs_pb_70">
             <div class="container">
@@ -126,6 +126,6 @@
         </div>
     </section>
     <!--=========================
-                                                    SIGNIN END
-                                                ==========================-->
+                                                                SIGNIN END
+                                                            ==========================-->
 @endsection
